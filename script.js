d314a3a5d5bc24850be1f9765c8b64ed2c6457c5
Evan McIntire <mcintire.evan@gmail.com>
2019-11-07 00:42:23 -0500

    Add prospectus, update brand, format all files

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function isValidEmail(email) {
-    return (/(.+)@(.+){2,}\.(.+){2,}/.test(email));
+  return /(.+)@(.+){2,}\.(.+){2,}/.test(email);
 }
 
 function onEmailEdit(input) {
-    document.getElementById("mc-embedded-subscribe").disabled = !isValidEmail(input);
+  document.getElementById("mc-embedded-subscribe").disabled = !isValidEmail(
+    input
+  );
 }
 
 function onSubmit() {
-    document.getElementById("mc_embed_signup").style.display = "none";
-    document.getElementById("confirmation").style.display = "block";
+  document.getElementById("mc_embed_signup").style.display = "none";
+  document.getElementById("confirmation").style.display = "block";
 }
